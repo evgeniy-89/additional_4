8ebccc3efd7b5b208fa034f00e07c196d5c1eaaf
Dzmitry Kazbiarovich <roman.evgeniy@gmail.com>
2017-09-27 22:30:47 +0300

    product task update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-module.exports = function multiply(first, second) {
-  // your solution
+module.exports = function multiply(a, b) {
+    a = a.split('').reverse();
+    b = b.split('').reverse();
+    let result = [];
+
+    for (let i = 0; i < a.length; i++) {
+        for (let j = 0; j < b.length; j++) {
+            if (!result[i + j]) {
+                result[i + j] = 0;
+            }
+
+            result[i + j] += a[i] * b[j];
+        }
+    }
+
+    for (let i = 0; i < result.length; i++) {
+        if (result[i] >= 10) {
+            if (!result[i + 1]) {
+                result[i + 1] = 0;
+            }
+
+            result[i + 1] += parseInt(result[i] / 10);
+            result[i] %= 10;
+        }
+    }
+
+    return result.reverse().join('');
 }
